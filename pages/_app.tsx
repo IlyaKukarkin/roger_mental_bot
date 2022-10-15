5b5935a3d8456101aea9bea9e77ee8985e48d00f
ilya kukarkin <i.kukarkin@xsolla.com>
2022-10-16 00:13:38 +0600

    add WebVitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export { reportWebVitals } from "next-axiom";
 
 export default MyApp;
