4a76d6f0d60492fb4fa7030019d71015c869cbbf
ilya kukarkin <i.kukarkin@xsolla.com>
2022-10-27 18:57:18 +0600

    test docker compose

--- python/jimmy/handlers/version.py
+++ python/jimmy/handlers/version.py
@@ -5,4 +5,4 @@ from singleton import Bot
 async def version_handler(message: types.Message):
     bot = Bot().get_bot()
 
-    await bot.send_message(message.chat.id, "Версия бота Джимми: 0.2.3")
+    await bot.send_message(message.chat.id, "Версия бота Джимми: 0.2.4")
--- python/redeploy.sh
+++ python/redeploy.sh
@@ -1,12 +1,15 @@
 #!bin/bash
 
-sudo docker pull ikukarkin/roger:roger
-sudo docker pull ikukarkin/roger:jimmy
+# sudo docker pull ikukarkin/roger:roger
+# sudo docker pull ikukarkin/roger:jimmy
 
-sudo docker stop roger
-sudo docker stop jimmy
+# sudo docker stop roger
+# sudo docker stop jimmy
 
-sudo docker system prune -f
+# sudo docker system prune -f
 
-sudo docker run -d -e DOPPLER_TOKEN=$1 --name=roger ikukarkin/roger:roger
-sudo docker run -d -e DOPPLER_TOKEN=$1 --name=jimmy ikukarkin/roger:jimmy
+# sudo docker run -d -e DOPPLER_TOKEN=$1 --name=roger ikukarkin/roger:roger
+# sudo docker run -d -e DOPPLER_TOKEN=$1 --name=jimmy ikukarkin/roger:jimmy
+
+sudo docker compose pull
+sudo docker compose up -d
