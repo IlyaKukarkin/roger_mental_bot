31e13b836b6f874e6f40d137dbf78416ce4519f6
ilya kukarkin <i.kukarkin@xsolla.com>
2022-11-11 00:41:57 +0600

    fix for users with now messages

--- python/roger/common.py
+++ python/roger/common.py
@@ -11,14 +11,18 @@ from config import bot, contentful_api_readonly_url, contenful_access_token, con
 with open('texts.json') as t:
     texts = json.load(t)
 
-#удаляем клавиатуру у сообщения после клика пользователя по ней
+# удаляем клавиатуру у сообщения после клика пользователя по ней
+
+
 async def delete_keyboard(chat_id: int, message_id: int):
     try:
         await bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id, reply_markup=None)
     except (Exception):
         return
 
-#получить картинку из хранилища по id
+# получить картинку из хранилища по id
+
+
 async def get_pictures(picture_id: str):
     response = requests.get(contentful_api_readonly_url + 'spaces/' + contenful_space_id +
                             '/environments/master/assets/' + picture_id + '?access_token=' + contenful_access_token)
@@ -26,7 +30,9 @@ async def get_pictures(picture_id: str):
     answer = answer.get("fields").get("file").get("url")
     return str(answer[2:])
 
-#получить массив текстов из json
+# получить массив текстов из json
+
+
 async def get_options(array: str):
     arr = []
     for item in texts.get(array):
@@ -34,7 +40,9 @@ async def get_options(array: str):
     s = await rand_select_obj_texts(arr)
     return s.get('text')
 
-#получить частоту текстов из json
+# получить частоту текстов из json
+
+
 async def rand_select_obj_texts(arr: list):
     rand_id_array = []
     j = 0
@@ -44,13 +52,17 @@ async def rand_select_obj_texts(arr: list):
         j += 1
     return arr[rand_id_array[random.randint(0, len(rand_id_array) - 1)]]
 
-#проверить валидность имени пользователя перед добавлением его в базу (исключаем вариант, когда username в телеграме не задан)
+# проверить валидность имени пользователя перед добавлением его в базу (исключаем вариант, когда username в телеграме не задан)
+
+
 async def check_id_username_is_valid_before_save(username: str):
     if (username == None):
         return ""
     return username
 
-#проверяем, была ли отправка сообщения пользователю сегодня
+# проверяем, была ли отправка сообщения пользователю сегодня
+
+
 async def is_any_messages_sent_today(user_id: ObjectId):
     collection_name = get_database()
     mental_rates = collection_name['mental_rate'].aggregate(
@@ -97,56 +109,57 @@ async def is_any_messages_sent_today(user_id: ObjectId):
     collection_name['mental_rate'].find().close()
     return result
 
-async def check_if_delete_mental_keyboard(user_id: ObjectId): 
+
+async def check_if_delete_mental_keyboard(user_id: ObjectId):
     collection_name = get_database()
     mental_hours = collection_name['mental_rate'].aggregate(
-[
-    {
-        '$match': {
-            'id_user': user_id
-        }
-    }, {
-        '$match': {
-            'rate': 0
-        }
-    }, {
-        '$addFields': {
-            'datetime_now': datetime.datetime.now(pytz.utc)
-        }
-    }, {
-        '$addFields': {
-            'date_diff': {
-                '$dateDiff': {
-                    'startDate': '$date', 
-                    'endDate': '$datetime_now', 
-                    'unit': 'hour'
+        [
+            {
+                '$match': {
+                    'id_user': user_id
                 }
+            }, {
+                '$match': {
+                    'rate': 0
+                }
+            }, {
+                '$addFields': {
+                    'datetime_now': datetime.datetime.now(pytz.utc)
+                }
+            }, {
+                '$addFields': {
+                    'date_diff': {
+                        '$dateDiff': {
+                            'startDate': '$date',
+                            'endDate': '$datetime_now',
+                            'unit': 'hour'
+                        }
+                    }
+                }
+            }, {
+                '$sort': {
+                    'date_diff': 1
+                }
+            }, {
+                '$limit': 1
             }
-        }
-    }, {
-        '$sort': {
-            'date_diff': 1
-        }
-    }, {
-        '$limit': 1
-    }
-]
+        ]
     )
+
     mental_hours_clone = list(mental_hours)
-    if (mental_hours_clone[0]['date_diff']==3):
-        user = collection_name["users"].find_one(
-            {"_id": user_id}, {'telegram_id': 1})
-        await bot.send_message(int(user['telegram_id']), await get_options('hurry_up_message'))
-        collection_name['users'].find().close()
+    
+    if (mental_hours_clone := None):
+        if (mental_hours_clone[0]['date_diff']):
+            if (mental_hours_clone[0]['date_diff'] == 3):
+                user = collection_name["users"].find_one(
+                    {"_id": user_id}, {'telegram_id': 1})
+                await bot.send_message(int(user['telegram_id']), await get_options('hurry_up_message'))
+                collection_name['users'].find().close()
 
-    if (mental_hours_clone[0]['date_diff']==4):
-        user = collection_name["users"].find_one(
-            {"_id": user_id}, {'telegram_id': 1})
-        await delete_keyboard(int(user['telegram_id']), int(list(mental_hours_clone)[0]['id_tg_message']))
-        await bot.send_message(int(user['telegram_id']), await get_options('thats_it_message'))
-        collection_name['users'].find().close()
+            if (mental_hours_clone[0]['date_diff'] == 4):
+                user = collection_name["users"].find_one(
+                    {"_id": user_id}, {'telegram_id': 1})
+                await delete_keyboard(int(user['telegram_id']), int(list(mental_hours_clone)[0]['id_tg_message']))
+                await bot.send_message(int(user['telegram_id']), await get_options('thats_it_message'))
+                collection_name['users'].find().close()
     collection_name['mental_rate'].find().close()
-
-
-
-
