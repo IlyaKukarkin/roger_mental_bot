4de18e4bae1a09265ea536d7a7c8ec77c5281569
Максим <maximkataev@yandex.ru>
2022-12-05 00:36:01 +0100

    botblocked and exception catch added on on_startup

--- python/roger/on_startup.py
+++ python/roger/on_startup.py
@@ -4,6 +4,8 @@ import asyncio
 import pytz
 from sendmessage import sendmes
 from common import is_any_messages_sent_today, check_if_delete_mental_keyboard, delete_keyboard
+from aiogram.utils.exceptions import BotBlocked
+
 
 async def enable_task_to_send_mes():
     while True:
@@ -52,11 +54,16 @@ async def enable_task_to_send_mes():
                 }
             }
         ])
-        for user in users:
-            await check_if_delete_mental_keyboard(user['_id'])
-            already_sent = await is_any_messages_sent_today(user['_id'])
-            if (not already_sent):
-                await sendmes(int(user['telegram_id']))
+        try:
+            for user in users:
+                await check_if_delete_mental_keyboard(user['_id'])
+                already_sent = await is_any_messages_sent_today(user['_id'])
+                if (not already_sent):
+                    await sendmes(int(user['telegram_id']))
+        except (BotBlocked): #если юзер заблочил бота, не падаем
+            print("Юзер " + user['telegram_id'] + "пидор, заблочил бота")
+        except (Exception):
+            print ("Failed to send a message to a user " + user['telegram_id'])
         collection_name['users'].find().close()
         collection_name['user_messages'].find().close()
         await asyncio.sleep(60*60)
