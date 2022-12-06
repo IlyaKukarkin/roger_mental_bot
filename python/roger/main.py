e35136c5708a54f0c009e00016b7eb0b290c91a6
Максим <maximkataev@yandex.ru>
2022-12-06 22:29:15 +0100

    money-mes-changed

--- python/roger/main.py
+++ python/roger/main.py
@@ -137,7 +137,7 @@ async def process_sendmes_command(message: types.Message):
 
 @dp.message_handler(commands=['money'])
 async def process_sendmes_command(message: types.Message):
-    await bot.send_message(message.chat.id, "Держите 5 денег")
+    await bot.send_message(message.chat.id, "Задонатить Роджеру: https://www.tinkoff.ru/cf/9KODrlaoPCR. Деньги будут потрачены на более мощный сервер 🔥")
 
 #регистрация пользователя
 #получаем имя пользователя
