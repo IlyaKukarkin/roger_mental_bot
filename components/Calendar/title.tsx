2088e9cc25deee99034d33f82bfa511eff60d7cc
Кукаркин Илья <ilya1196@yandex.ru>
2023-06-29 15:52:32 +0800

    Fix statistic image (#120)
    * Fix user stats image
    
    * Add prettier script
    
    * Format files
    
    ---------
    
    Co-authored-by: ilya kukarkin <i.kukarkin@xsolla.com>

--- components/Calendar/title.tsx
+++ components/Calendar/title.tsx
@@ -1,15 +1,19 @@
-import React from "react"
+import React from "react";
 
 type Props = {
-    text: string;
-}
+  text: string;
+};
 
 const Title = ({ text }: Props) => {
-    return (
-        <div tw={'flex ml-2 mt-3 opacity-80 w-10 h-10 justify-center items-center rounded-full'}>
-            <p tw="text-gray-100 font-bold text-xl">{text}</p>
-        </div>
-    )
-}
+  return (
+    <div
+      tw={
+        "flex ml-2 mt-3 opacity-80 w-10 h-10 justify-center items-center rounded-full"
+      }
+    >
+      <p tw="text-gray-100 font-bold text-xl">{text}</p>
+    </div>
+  );
+};
 
-export default Title
+export default Title;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,9 +18,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="robots" content="index, follow" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="Russian" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#041347" />
         <meta name="msapplication-TileColor" content="#041347" />
