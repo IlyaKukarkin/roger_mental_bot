632cd8c338de22490a19abccf943961f05fae4d9
Максим <maximkataev@yandex.ru>
2023-07-30 20:37:03 +0400

    123

--- python/roger/sendmessage.py
+++ python/roger/sendmessage.py
@@ -60,8 +60,8 @@ async def callback_after_click_on_color_button(callback_query: types.CallbackQue
     await delete_keyboard(callback_query.from_user.id, callback_query.message.message_id)
     try:
         #добавил две строки ниже, чтобы по коллбеку стирать контекст общения с чатжпт, чтобы он не копился
-        array_of_chats.delete_array(callback_query.from_user.id)
-        array_of_chats.add_message (callback_query.from_user.id, {'role': 'assistant', 'content': 'Отвечай от имени Роджера. Это бот, который поддерживает людей с плохим настроением'})
+        # array_of_chats.delete_array(callback_query.from_user.id)
+        # array_of_chats.add_message (callback_query.from_user.id, {'role': 'assistant', 'content': 'Отвечай от имени Роджера. Это бот, который поддерживает людей с плохим настроением'})
         collection_name = get_database()
         user = collection_name["users"].find_one( 
             {"telegram_id": str(callback_query.from_user.id)}, {'_id': 1, 'name': 0})
