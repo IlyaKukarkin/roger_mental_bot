fce81f5193bd6fd5fc447e8afafe939c331da048
ilya kukarkin <i.kukarkin@xsolla.com>
2023-11-14 23:59:49 +0800

    add Messages DB file

--- python/roger/db/users.py
+++ python/roger/db/users.py
@@ -1,4 +1,5 @@
-"""Module providing a date time functions."""
+"""Module providing functions for accessing Users table from DB."""
+
 import datetime
 from bson import ObjectId
 
@@ -13,7 +14,7 @@ def insert_new_user(
     user_time: str
 ):
     """
-    Adds a new user record to the DataBase table "Users";
+    Adds a new user record to the DataBase table "Users"
 
     Parameters:
     tg_username (str): username from Telegram, starts with an "@"
@@ -43,11 +44,30 @@ def insert_new_user(
     })
 
 
+def update_user_is_active(
+    _id: ObjectId,
+    is_active: bool
+):
+    """
+    Updates the "is_active" field for user by his "_id"
+
+    Parameters:
+    _id (ObjectId): ID for the user to update
+    is_active (bool): value for "is_active" to set
+
+    Returns:
+    None
+    """
+
+    dbClient['users'].find_one_and_update(
+        {'_id': _id}, {"$set": {'is_active': is_active}})
+
+
 def get_user_by_tg_username(
     tg_username: str,
 ):
     """
-    Returns an user record from the DataBase table "Users" by his Telegram username;
+    Returns an user record from the DataBase table "Users" by his Telegram username
 
     Parameters:
     tg_username (str): username from Telegram, starts with an "@"
@@ -65,7 +85,7 @@ def get_user_by_id(
     _id: ObjectId,
 ):
     """
-    Returns an user record from the DataBase table "Users" by his "_id";
+    Returns an user record from the DataBase table "Users" by his "_id"
 
     Parameters:
     _id (ObjectId): ID for the user to find
@@ -75,5 +95,100 @@ def get_user_by_id(
     """
 
     user = dbClient['users'].find_one({"_id": _id})
+
+    return user
+
+
+def get_user_by_telegram_id(
+    telegram_id: str,
+):
+    """
+    Returns an user record from the DataBase table "Users" by his "telegram_id"
+
+    Parameters:
+    telegram_id (str): Telegram ID for the user to find
+
+    Returns:
+    dict: User
+    """
+
+    user = dbClient['users'].find_one({"telegram_id": str(telegram_id)})
+
+    return user
+
+
+def get_all_admins():
+    """
+    Returns an array of all admin users from the DataBase table "Users"
+
+    Parameters:
+
+    Returns:
+    array: Users
+    """
+
+    admin_users = dbClient['users'].find({"is_admin": True, "is_active": True})
+
+    return admin_users
+
+
+def get_all_active_users():
+    """
+    Returns an array of all active users from the DataBase table "Users"
+
+    Parameters:
+
+    Returns:
+    array: Users
+    """
+
+    all_users = dbClient['users'].find({"is_active": True})
+
+    return all_users
+
+
+def get_user_with_mental_rate(
+    _id: ObjectId,
+    from_date: datetime
+):
+    """
+    Returns an user by "_id" with all mental rates from specific date
+
+    Parameters:
+    _id (ObjectId): ID for the user to find
+    from_date (datetime): date from which aggregate "mental_rate"
+
+    Returns:
+    dict: User
+        rates: array of "Mental Rate"
+    """
+
+    user = dbClient['users'].aggregate(
+        [
+            {
+                '$match': {
+                    '_id': _id
+                }
+            }, {
+                '$lookup': {
+                    'from': 'mental_rate',
+                    'localField': '_id',
+                    'foreignField': 'id_user',
+                    'pipeline': [
+                        {
+                            '$match': {
+                                '$expr': {
+                                    '$gte': [
+                                        '$date', from_date
+                                    ]
+                                }
+                            }
+                        }
+                    ],
+                    'as': 'rates'
+                }
+            }
+        ]
+    )
 
     return user
