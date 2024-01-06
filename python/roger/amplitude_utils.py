439fe7cd905182f3787494c6ad378069108ae438
Maksim Kataev <maximkataev@yandex.ru>
2024-01-06 19:37:44 +0400

    Roger amplitude new events (#200)
    * reg fixes
    
    * new events added

--- python/roger/amplitude_utils.py
+++ python/roger/amplitude_utils.py
@@ -8,18 +8,32 @@ from amplitude import BaseEvent
 amplitude_api_key = os.getenv("AMPLITUDE_API_KEY")
 amplitude = Amplitude(amplitude_api_key)
 
+async def amplitude_send_default_source_event(event_type: str, user_id: str, message: str, extra: str):
+    """Function to send events to Amplitude"""
+
+    amplitude.track(
+        BaseEvent(
+            event_type=event_type,
+            user_id=user_id,
+            event_properties={
+                "message": message,
+                "extra": extra
+            }
+        )
+    )
 
 async def amplitude_send_start_source_event(user_id: str, source: str, extra: str):
-    """Function to send events to amplitude"""
+    """Function to send start events to Amplitude"""
 
     amplitude.track(
         BaseEvent(
             event_type="Start Event",
-            user_id=str(user_id),
+            user_id=user_id,
             event_properties={
-                "user": {"_id": user_id},
                 "source": source,
                 "extra": extra
             }
         )
     )
+
+
--- python/roger/handlers.py
+++ python/roger/handlers.py
@@ -7,6 +7,7 @@ from db.rate import insert_new_rate
 from db.user_messages import get_user_message_by_tg_message
 from variables import botClient
 from common import delete_keyboard
+from amplitude_utils import amplitude_send_default_source_event
 
 
 async def rate_message(callback_query: types.CallbackQuery, rate: bool):
@@ -48,3 +49,4 @@ async def rate_message(callback_query: types.CallbackQuery, rate: bool):
                 "напиши разработчикам через команду /feedback"
             )
         )
+        await amplitude_send_default_source_event("Error", str(callback_query.from_user.id), "rate_message", "PyMongoError")
